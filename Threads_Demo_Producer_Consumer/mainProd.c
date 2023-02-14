0348f9815ca923209736c34a1817b6e8599105fe
Yash Joshi <yashj@juniper.net>
2023-02-14 15:23:02 +0530

    Minor updates to the code.

--- Threads_Demo_Producer_Consumer/mainProd.c
+++ Threads_Demo_Producer_Consumer/mainProd.c
@@ -6,15 +6,19 @@ int main()
   int shm_id; //variable to save the shared memory returned from the shmget.
 
 
-  /*------------Shared memory get and attach------------*/
-  shm_id = shmget(KEY , 4096 , IPC_CREAT | 0600); // create and/or get the shared memory
+  /* Shared memory get and attach
+   */
+
+  //create and/or get the shared memory
+  shm_id = shmget(KEY , 4096 , IPC_CREAT | 0600);
   if (shm_id < 0 )
   {
     perror("error in creating the shared memory");
     exit(1);
   }
 
-  shma = shmat(shm_id, 0, 0); // attach the shared memory to the virtual memory space of the current Process.
+  // attach the shared memory to the virtual memory space of the current Process.
+  shma = shmat(shm_id, 0, 0);
   if (shma < 0)
   {
     perror("error in Attaching the Shared Memory");
@@ -22,14 +26,19 @@ int main()
   }
 
   
-  /*------------Thread Semaphore initialization------------*/
-  ret = sem_init( &(shma->sema1), 2, 1); // initialize the thread Semaphore which is inside the shared memory.
+  /* Thread Semaphore initialization
+   */
+
+  // initialize the thread Semaphore which is inside the shared memory.
+  ret = sem_init( &(shma->sema1), 2, 1);
   if (ret < 0 )
   {
     perror("error in Initializing of the Thread Semaphore.");
     exit(3);
   }
-  ret = sem_init( &(shma->sema2), 2, 0);// initialize the thread Semaphore which is inside the shared memory.
+
+  // initialize the thread Semaphore which is inside the shared memory.
+  ret = sem_init( &(shma->sema2), 2, 0);
   if ( ret < 0 )
   {
     perror("error in Initializing of the Thread Semaphore.");
@@ -37,8 +46,11 @@ int main()
   }
 
   
-  /*------------Thread Mutex initialization------------*/
-  pthread_mutex_init( &(shma->mutex) , NULL );// initialize the thread Mutex which is inside the shared memory.
+  /* Thread Mutex initialization
+   */
+
+  // initialize the thread Mutex which is inside the shared memory.
+  pthread_mutex_init( &(shma->mutex) , NULL );
   if ( ret < 0 )
   {
     perror("error in Initializing of the Thread Semaphore.");
@@ -50,14 +62,17 @@ int main()
   shma->BufSizeMax = 25;  // initializing the Max_Buffer_Size of the User space pipe IPC
   shma->UsedSlotCount = 0;  // initializing the Used_Slot_Count of the User space pipe IPC
 
-  ret = pthread_create( &ThId , NULL , threadProd , NULL); // Create a Producerthread.
+  // Create a Producerthread.
+  ret = pthread_create( &ThId , NULL , threadProd , NULL);
   if (ret != 0)
   {
     perror("Error in thread Creation for ThreadProducer:\n");
     printf("Error Number:%d\n",ret);
     exit(ret);
   }
-  pthread_join( ThId , NULL); // This process/application will not be terminated without terminating rthe thread it has started.
+
+  // This process/application will not be terminated without terminating the thread it has started.
+  pthread_join( ThId , NULL);
 
   exit(0);
 
@@ -65,31 +80,51 @@ int main()
 
 
 void * threadProd(void *ProdVPtr){
-  char value; // creating a buffer variable to save the given input temporarily.
-    printf("---------Producer Entered CSection---------\nWaiting for the User to input Character or a String...\n"); // indicate that the thread has gotten access to the  Thread Semaphore.
+  // creating a buffer variable to save the given input temporarily.
+  char value;
+    // indicate that the thread has gotten access to the  Thread Semaphore.
+    printf("---------Producer Entered CSection---------\n \
+            Waiting for the User to input Character or a String...\n");
   
   while(1){
-    
-    sem_wait( &(shma->sema1) ); // Decrement the thread semaphore to enter the critical section and modify.
-    pthread_mutex_lock( &(shma->mutex) ); // lock the mutex to get extra protection.
+
+    // Decrement the thread semaphore to enter the critical section and modify.
+    sem_wait( &(shma->sema1) );
+
+    // lock the mutex to get extra protection.
+    pthread_mutex_lock( &(shma->mutex) );
     
     value = getchar();
 
     if (shma->UsedSlotCount < shma->BufSizeMax){
 
       shma->BufArea[shma->WrIndex] = value;
-      shma->WrIndex = (shma->WrIndex + 1) % shma->BufSizeMax; // as the buffer is circular I increment the write index untill it reaches BuffSizeMax, after that it will automatically be reinitialized to 0.
-      shma->UsedSlotCount++; //after reading one character, increment the Used Slot Count variable, because 1 character has been produced and the slot has been consumed.
+
+      // as the buffer is circular I increment the write index 
+      // untill it reaches BuffSizeMax,
+      // after that it will automatically be reinitialized to 0.
+      shma->WrIndex = (shma->WrIndex + 1) % shma->BufSizeMax;
+
+      // after reading one character,
+      // increment the Used Slot Count variable,
+      // because 1 character has been produced and the slot has been consumed.
+      shma->UsedSlotCount++;
     
     }
 
-    
-    pthread_mutex_unlock( &(shma->mutex) ) ; // UNlock the mutex which was used to get low-level protection.
-    sem_post( &(shma->sema2)); // Increment the other thread semaphore to exit the critical section and indicate the consumer to get active and start consuming the data..
+    // UnLock the mutex which was used to get low-level protection.
+    pthread_mutex_unlock( &(shma->mutex) ) ;
+
+    // Increment the other thread semaphore to exit the critical section and
+    // indicate the consumer to get active and start consuming the data..
+    sem_post( &(shma->sema2));
     
     
   }//threadProd:while:end
-    printf("---------Producer Exited CSection---------\n"); // indicates that the thread has exited the critical section and the consumer must consume the data now..
+
+  // indicates that the thread has exited the critical section and
+  // the consumer must consume the data now..
+  printf("---------Producer Exited CSection---------\n");
 
   pthread_exit(0);
 }//threadProd:end
